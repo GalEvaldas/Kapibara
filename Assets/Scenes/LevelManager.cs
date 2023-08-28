65149cc9af87d9ececb0437109ef5873b3221c50
GalEvaldas <127189513+GalEvaldas@users.noreply.github.com>
2023-08-28 10:12:39 +0300

    added 6 new levels

--- Assets/Scenes/LevelManager.cs
+++ Assets/Scenes/LevelManager.cs
@@ -29,4 +29,44 @@ public class LevelManager : MonoBehaviour
     {
         SceneManager.LoadScene("SettinsPage");
     }
+    public void changeScene4()
+    {
+        SceneManager.LoadScene("GamePage01");
+    }
+    public void changeScene5()
+    {
+        SceneManager.LoadScene("GamePage02");
+    }
+    public void changeScene6()
+    {
+        SceneManager.LoadScene("GamePage03");
+    }
+    public void changeScene7()
+    {
+        SceneManager.LoadScene("GamePage04");
+    }
+    public void changeScene8()
+    {
+        SceneManager.LoadScene("GamePage05");
+    }
+    public void changeScene9()
+    {
+        SceneManager.LoadScene("GamePage06");
+    }
+    public void changeScene10()
+    {
+        SceneManager.LoadScene("GamePage07");
+    }
+    public void changeScene11()
+    {
+        SceneManager.LoadScene("GamePage08");
+    }
+    public void changeScene12()
+    {
+        SceneManager.LoadScene("GamePage09");
+    }
+    public void changeScene13()
+    {
+        SceneManager.LoadScene("GamePage10");
+    }
 }
