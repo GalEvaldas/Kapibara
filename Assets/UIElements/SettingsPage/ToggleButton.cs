5e487bb2c5dd04996fc3506777ebd56e751d1f67
GalEvaldas <127189513+GalEvaldas@users.noreply.github.com>
2023-09-05 13:53:46 +0300

    updated

--- Assets/UIElements/SettingsPage/ToggleButton.cs
+++ Assets/UIElements/SettingsPage/ToggleButton.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToggleButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Sprite[] buttonSprites;
+    [SerializeField] Image targetButton;
+   
+    public void ChangeSprite()
     {
-        
+     if(targetButton.sprite == buttonSprites[0])
+        {
+            targetButton.sprite = buttonSprites[1];
+            return;
+        }
+        targetButton.sprite = buttonSprites[0];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
